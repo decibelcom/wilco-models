bf71617253e7cdbac8eb69927598b362327f7ee7
liel <liel.mimouni@outlook.com>
2016-09-27 17:54:21 +0200

    +

--- models/country.js
+++ models/country.js
@@ -2,7 +2,7 @@ module.exports = {
   initialize: function (attrs, options) {
     if(!attrs) return
 
-    var props = ['code', 'english_name']
+    var props = ['code', 'alpha2', 'alpha3', 'name']
 
     for(var i = 0; i < props.length; i++)
       if(attrs[props[i]]) this[props[i]] = attrs[props[i]]
