74afe7d1f5cbf2e328c90234a3222bd3445ad25a
liel <liel.mimouni@outlook.com>
2016-10-19 17:11:20 +0200

    +

--- models/permission-request.js
+++ models/permission-request.js
@@ -7,7 +7,7 @@ module.exports = {
   initialize: function (attrs, options) {
     if(!attrs) return
 
-    var props = ['user', 'accepted', 'treated']
+    var props = ['user', 'accepted', 'treated', 'actions']
 
     for(var i = 0; i < props.length; i++)
       if(attrs[props[i]]) this[props[i]] = attrs[props[i]]
